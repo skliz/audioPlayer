bb3d91b6ddb7b024bba4694a4bd44fd913257ed3
Fedor <skliz-33@mail.ru>
2023-06-12 21:55:18 +0300

    Add files via upload

---
+++ js/AudioPlayer.js
@@ -0,0 +1,99 @@
+export class AudioPlayer {
+	// массив песен
+	songs = [`Asia`, `Ptichka`, `VdolDorog`, `Danse`, `Ysoldat`]
+	// Индекс
+	songIndex = 0
+	constructor(player, audio, playerImage, playerSong, progressContainer, progress, btnPrev, btnPlay, btnNext) {
+		Object.assign(this, { player, audio, playerImage, playerSong, progressContainer, progress, btnPrev, btnPlay, btnNext })
+
+		// load
+		this.loadSong(this.songs[this.songIndex])
+
+		// play
+		this.btnPlay.addEventListener('click', () => {
+			const isPlaing = this.player.classList.contains('play')
+			if (isPlaing) {
+				this.pauseSong()
+			} else {
+				this.playSong()
+			}
+		})
+
+		// next
+		this.btnNext.addEventListener('click', () => {
+			this.nextSong()
+		})
+
+		// prev
+		this.btnPrev.addEventListener('click', () => {
+			this.prevSong()
+		})
+
+		// progress
+		this.audio.addEventListener('timeupdate', this.updateProgress)
+
+		// set progress(перемотка)
+		this.progressContainer.addEventListener('click', (e) => {
+			this.setProgress(e)
+		})
+
+		// autoplay
+		this.audio.addEventListener('ended', () => {
+			this.nextSong()
+		})
+	}
+	// -------------------------------------------------------------------------------------------------------------
+	// load
+	loadSong(song) {
+		this.playerSong.textContent = song
+		this.audio.src = `audio/${song}.mp3`
+		this.playerImage.src = `img/${song}${this.songIndex + 1}.jpeg`
+	}
+
+	// play
+	playSong() {
+		this.player.classList.add('play')
+		this.audio.play()
+	}
+
+	// pause
+	pauseSong() {
+		this.player.classList.remove('play')
+		this.audio.pause()
+	}
+
+	// next song
+	nextSong() {
+		this.songIndex++
+		if (this.songIndex > this.songs.length - 1) {
+			this.songIndex = 0
+		}
+		this.loadSong(this.songs[this.songIndex])
+		this.playSong()
+	}
+
+	// prev song
+	prevSong() {
+		this.songIndex--
+		if (this.songIndex == 0) {
+			this.songIndex = this.songs.length - 1
+		}
+		this.loadSong(this.songs[this.songIndex])
+		this.playSong()
+	}
+
+	// progressBar
+	updateProgress(e) {
+		const { duration, currentTime } = e.srcElement
+		const progressPercent = (currentTime / duration) * 100
+		progress.style.width = `${progressPercent}%`
+	}
+
+	// set progress(перемотка);
+	setProgress(e) {
+		const width = e.target.clientWidth
+		const clickX = e.offsetX
+		const duration = audio.duration
+		this.audio.currentTime = (clickX / width) * duration
+	}
+}
---
+++ js/script.js
@@ -0,0 +1,61 @@
+import { AudioPlayer } from "./AudioPlayer.js"
+
+// AudioPlayer
+const player = new AudioPlayer(
+	document.getElementById('player'),
+	document.getElementById('audio'),
+	document.getElementById('player-image'),
+	document.getElementById('player-song'),
+	document.getElementById('progress-container'),
+	document.getElementById('progress'),
+	document.getElementById('btn-prev'),
+	document.getElementById('btn-play'),
+	document.getElementById('btn-next'),
+)
+
+// canvas
+document.addEventListener('DOMContentLoaded', function (e) {
+
+	const canvas = document.getElementById('canvas')
+	const audio = document.getElementById('audio')
+	const ctx = canvas.getContext('2d')
+	const columnGap = 2
+	const columnCount = 128 // Кол-во колонок: 1024, 512, 256, 128, 64
+
+	let audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+	let source = audioCtx.createMediaElementSource(audio)
+	let analyser = audioCtx.createAnalyser()
+	analyser.fftSize = columnCount
+	source.connect(analyser) // Подключаем анализатор к элементу audio
+	analyser.connect(audioCtx.destination) // Без этой строки нет звука но анализатор работает
+	let frequencyData = new Uint8Array(analyser.frequencyBinCount)
+
+	// draws a column
+	function drawColumn(x, width, height) {
+		const gradient = ctx.createLinearGradient(0, canvas.height - height / 2, 0, canvas.height)
+		gradient.addColorStop(1, `rgba(255,255,255,1)`)
+		gradient.addColorStop(0.9, `rgba(255,150,0,1)`)
+		gradient.addColorStop(0, `rgba(255,0,0,0)`)
+		ctx.fillStyle = gradient
+		ctx.fillRect(x, canvas.height - height / 2, width, height)
+	}
+
+	// render
+	function render() {
+		analyser.getByteFrequencyData(frequencyData) // Записываем в массив
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		// Ширина колонки
+		const columnWidth = (canvas.width / frequencyData.length) - columnGap + (columnGap / frequencyData.length)
+		// Масштабный коофицент
+		const heightScale = canvas.height / 100
+		// Позиция каждой колонки
+		let xPos = 0
+		for (let i = 0; i < frequencyData.length; i++) {
+			let columnHeight = frequencyData[i] * heightScale
+			drawColumn(xPos, columnWidth, columnHeight / 2)
+			xPos += columnWidth + columnGap
+		}
+		window.requestAnimationFrame(render)
+	}
+	window.requestAnimationFrame(render)
+})
